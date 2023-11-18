677bdc7323d7b09339958a5b875205730415a202
Mujib Tocalo <mujib@DESKTOP-LVBQ85J>
2023-11-18 20:40:50 +0800

    dynamic navbar

--- MSU-EDAAMS/src/components/Navbar.jsx
+++ MSU-EDAAMS/src/components/Navbar.jsx
@@ -179,7 +179,7 @@ export const CustomNavbar = ({ setOpen }) => {
   });
 
   return (
-    <Navbar className="flex max-w-screen items-center mx-auto justify-between  p-1 my-1">
+    <div className="flex max-w-screen items-center rounded-lg shadow-lg justify-between p-1 mb-2">
       <Typography className="flex flex-row gap-4 items-center ml-2 text-lg p-0.5 pr-3 text-center font-semibold text-white bg-indigo-800 rounded-lg">
         <TiThMenuOutline onClick={() => setOpen((prevOpen) => !prevOpen)} className="flex bg-indigo-800 rounded-lg w-8 h-8 p-1 translate-x-2 cursor-pointer hover:scale-110" />
         MSU EDAAMS
@@ -197,12 +197,12 @@ export const CustomNavbar = ({ setOpen }) => {
             </div>
           </div> */}
         <div className="flex flex-col justify-center items-center bg-indigo-800 rounded-xl p-1 px-2 cursor-default">
-          <Typography className="flex font-md">
+          <Typography className="flex font-md text-white">
             {currentUser + " | " + userDesignation + ' | ' + userCollege}
           </Typography>
         </div>
         <ProfileMenu />
       </div>
-    </Navbar>
+    </div>
   );
 };
